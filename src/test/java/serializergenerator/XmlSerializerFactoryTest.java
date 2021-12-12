8a53dd91b49378e98ca41f8ef263892113560138
equocredite <equocredite@gmail.com>
2021-12-12 21:29:33 +0300

    fix junit annotation

--- src/test/java/serializergenerator/XmlSerializerFactoryTest.java
+++ src/test/java/serializergenerator/XmlSerializerFactoryTest.java
@@ -1,6 +1,7 @@
 package serializergenerator;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -62,7 +63,7 @@ class Car {
 public class XmlSerializerFactoryTest {
     private Serializer<Person> serializer;
 
-    @BeforeAll
+    @BeforeEach
     void setup() {
         serializer = new XmlSerializerFactory<>(Person.class).createSerializer();
     }
